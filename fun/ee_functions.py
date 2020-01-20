f9594b30917e5165d4f35a02a548e2591c0684a6
maawoo <marco.wolsza@gmail.com>
2020-01-20 14:40:06 +0100

    Added function descriptions & Minor changes overall

--- fun/ee_functions.py
+++ fun/ee_functions.py
@@ -4,32 +4,58 @@ import pandas as pd
 import datetime
 
 def process_geojson(geojson, box_yn, box_size=1):
-    """
+    """Creates a list of Earth Engine (EE) geometry objects from ISMN station
+    coordinates.
+
+    :param geojson: Imported GeoJSON file that includes ISMN station
+    coordinates.
+    :param box_yn: (From user input) Value should be 0 or 1. Depending on
+    the value, either point objects (0) or polygons that enclose the
+    station coordinates (1) are returned.
+    :param box_size: (From user input) Value should be an integer >= 0.
+    Defines the polygon size surrounding the station coordinates and will be
+    used for the extraction of the mean backscatter of the defined region.
+    :return: List of EE geometry objects.
     """
     geo_list = []
     if box_yn == 0:
         for i in range(len(geojson)):
-            geo_list.append(ee.Geometry.Point(geojson["geometry"][i].y, geojson["geometry"][i].x))
-
+            geo_list.append(ee.Geometry.Point(geojson["geometry"][i].y,
+                                              geojson["geometry"][i].x))
     elif box_yn == 1:
         for i in range(len(geojson)):
-            geo_list.append(ee.Geometry.Point(geojson["geometry"][i].y, geojson["geometry"][i].x).buffer(box_size/2)
+            geo_list.append(ee.Geometry.Point(geojson["geometry"][i].y,
+                                              geojson["geometry"][i].x)
+                            .buffer(box_size/2)
                             .bounds())
-
+    else:
+        print("Something is wrong! The variable box_yn should be 0 (extract "
+              "backscatter for pixel coordinates) or 1 ( "
+              "extract mean backscatter for a bounding box).")
     return geo_list
 
 
 def lc_filter(geo_list):
-    """
-    description of the function
+    """Filters a list of Earth Engine (EE) geometry objects by checking the
+    land cover type of each location.
+    Land cover dataset: https://tinyurl.com/cgls-lc100
+    Valid land cover types:
+    40 = Cultivated and managed vegetation / agriculture
+    60 = Bare / sparse vegetation
+
+    :param geo_list: List of EE geometry objects.
+    :return: Filtered list of EE geometry objects.
     """
     lc = ee.ImageCollection("COPERNICUS/Landcover/100m/Proba-V/Global").first()
-    valid_ids = [20, 30, 40, 60, 100]
+    valid_ids = [40, 60]
     geo_list_filt = []
 
     for geo in range(len(geo_list)):
-        lc_value = lc.select("discrete_classification").reduceRegion(ee.Reducer.first(), geo_list[geo], 10).get(
-            "discrete_classification").getInfo()
+        lc_value = lc.select("discrete_classification")\
+            .reduceRegion(ee.Reducer.first(), geo_list[geo], 10)\
+            .get("discrete_classification")\
+            .getInfo()
+
         if lc_value in valid_ids:
             geo_list_filt.append(geo_list[geo])
 
@@ -37,22 +63,26 @@ def lc_filter(geo_list):
 
 
 def get_image_collection(geo_list_filt):
-    """
-    description of the function
+    """Gets the available Sentinel-1 image collection (ascending and
+    descending) for each Earth Engine (EE) geometry object of a list.
+
+    :param geo_list_filt: Filtered list of EE geometry objects.
+    :return: Dictionary with EE geometry object, descending and ascending
+    S-1 image collections as values.
     """
     imagecoll = {}
+    mode = ee.Filter.eq('instrumentMode', 'IW')
+    res = ee.Filter.eq('resolution', 'H')
 
-    ## Set additional filters
-    mode = ee.Filter.eq('instrumentMode', 'IW')  # IW = Interferometric Wide Swath
-    res = ee.Filter.eq('resolution', 'H')  # H = High
-
-    ## Apply filters to image collection for each location and save results in dictionary
     for geo in range(len(geo_list_filt)):
-        s1 = ee.ImageCollection('COPERNICUS/S1_GRD').filter(mode).filter(res).filterBounds(geo_list_filt[geo])
+        s1 = ee.ImageCollection('COPERNICUS/S1_GRD')\
+            .filter(mode)\
+            .filter(res)\
+            .filterBounds(geo_list_filt[geo])
+
         s1_desc = s1.filter(ee.Filter.eq('orbitProperties_pass', 'DESCENDING'))
         s1_asc = s1.filter(ee.Filter.eq('orbitProperties_pass', 'ASCENDING'))
 
-        ## Add data to dictionary
         imagecoll["geo_" + str(geo)] = [geo_list_filt[geo], s1_desc, s1_asc]
 
     return imagecoll
@@ -61,7 +91,8 @@ def get_image_collection(geo_list_filt):
 def get_s1_date(out):
     """ Obtains the SAR image acquisition date from the product ID
     Args:
-        out (List of dictionaries): Each dictionary corresponds to the sentinel1 info of an image
+        out (List of dictionaries): Each dictionary corresponds to the
+        sentinel1 info of an image
     Returns:
         Dates : A list of the acquisition dates
 
@@ -94,11 +125,11 @@ def get_s1_date(out):
 
 
 def simplify(fc):
-    """Take a feature collection, as returned by mapping a reducer to a ImageCollection,
-        and reshape it into a simpler list of dictionaries
+    """Take a feature collection, as returned by mapping a reducer to an
+    ImageCollection, and reshape it into a simpler list of dictionaries
     Args:
-        fc (dict): Dictionary representation of a feature collection, as returned
-            by mapping a reducer to an ImageCollection
+        fc (dict): Dictionary representation of a feature collection,
+        as returned by mapping a reducer to an ImageCollection
     Returns:
         list: A list of dictionaries.
 
@@ -128,13 +159,18 @@ def time_series_of_a_point(img_collection, point):
 
 
 def time_series_of_a_region(img_collection, geometry):
-
+    """
+    @author: crisj
+    Modified by: Marco Wolsza
+    """
     def _reduce_region(image):
-        """Spatial aggregation function for a single image and a polygon feature"""
-        # The reduction is specified by providing the reducer (ee.Reducer.mean()), the geometry  (a polygon coord),
-        # at the scale (30 meters)
-        # Feature needs to be rebuilt because the backend doesn't accept to map
-        # functions that return dictionaries
+        """
+        Spatial aggregation function for a single image and a polygon feature
+        """
+        # The reduction is specified by providing the reducer (
+        # ee.Reducer.mean()), the geometry  (a polygon coord), at the scale
+        # (30 meters) Feature needs to be rebuilt because the backend
+        # doesn't accept to map functions that return dictionaries
         stat_dict = image.reduceRegion(ee.Reducer.mean(), geometry, 30)
         return ee.Feature(None, stat_dict)
 
