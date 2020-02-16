fda6f549d390cca30c89f59c8697327ba170b648
maawoo <marco.wolsza@gmail.com>
2020-02-16 14:50:14 +0100

    Minor changes

--- 2_GEE.py
+++ 2_GEE.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import geopandas as gpd
 import ee
 from fun import ee_functions
@@ -11,9 +12,9 @@ ee.Initialize()
 
 while True:
     try:
-        box_yn = int(input("Do you want to extract backscatter values for "
-                           "the pixel coordinate (input: 0) or "
-                           "calculate the mean backscatter value for a box "
+        box_yn = int(input("Do you want to extract... \n backscatter values "
+                           "for the pixel coordinate (input: 0) \n or "
+                           "the mean backscatter value for a box "
                            "surrounding the pixel coordinate (input: "
                            "1)?"))
     except ValueError:
@@ -52,8 +53,8 @@ else:
 
 ee_geo_filt = ee_functions.lc_filter(ee_geo)
 print(str(len(ee_geo_filt)) +
-      " from " + str(len(ee_geo)) +
-      " locations left, after applying the land cover filter.")
+      " out of " + str(len(ee_geo)) +
+      " locations remain, after applying the land cover filter.")
 
 #############################################
 # Load and filter Sentinel-1 image collection
@@ -63,12 +64,16 @@ img_coll = ee_functions.get_image_collection(ee_geo_filt)
 #############################################
 # Extract backscatter data for each location
 
+stations = pd.read_csv("./data/stations.csv")
+stations
+
+
 # Point
 for key in img_coll.keys():
     s1_data = ee_functions.time_series_of_a_point(img_coll[key][1], img_coll[key][0])
     s1_data.reset_index().plot(x='Dates', y='VH')
-    plt.savefig("./figs/" + str(key) + ".png")
-    plt.close()
+    #plt.savefig("./figs/" + str(key) + ".png")
+    #plt.close()
 
 # Region
 for key in img_coll.keys():
@@ -77,11 +82,8 @@ for key in img_coll.keys():
     plt.savefig("./figs/" + str(key) + "_sq50.png")
     plt.close()
 
-
-#s1_data = ee_functions\
-#    .time_series_of_a_point(img_coll["geo_1"][1], img_coll["geo_1"][0])
-#s1_data
+s1_data = ee_functions.time_series_of_a_point(img_coll["geo_1"][1], img_coll["geo_1"][0])
+s1_data
 
 #s1_data.reset_index().plot(x='Dates', y='VH')
-#s1_data.reset_index().plot(x='Dates', y='VV')
 #plt.show()
