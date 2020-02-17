0dac9c71aa3f8eed1ff70f93445637cffb815c5a
maawoo <marco.wolsza@gmail.com>
2020-02-17 20:47:42 +0100

    Changed function import (because files were moved) and shortened function call prefixes

--- 1_PreProcess.py
+++ 1_PreProcess.py
@@ -1,15 +1,15 @@
 import csv
 from geojson import Point, Feature, FeatureCollection, dump
-from fun import preprocess
+from GEE_ISMN import preprocess as prep
 
-preprocess.setup_dir()
+prep.setup_dir()
 
 ## List all .stm files
-sm_files = preprocess.get_sm_files()
+sm_files = prep.get_sm_files()
 
 ## Filter soil moisture files by measurement depth
 depth = "0.05"
-sm_files_filt = preprocess.filter_files(sm_files, depth)
+sm_files_filt = prep.filter_files(sm_files, depth)
 
 
 coord_1 = []
@@ -19,7 +19,7 @@ features = []
 
 for i in sm_files_filt:
     checked_file_1 = open(i)
-    header_elements, filename_elements = preprocess.get_info_from_file(i)
+    header_elements, filename_elements = prep.get_info_from_file(i)
     coord_1.append(header_elements[3])
     coord_2.append(header_elements[4])
     station.append(
--- 2_GEE.py
+++ 2_GEE.py
@@ -1,24 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import geopandas as gpd
-from fun import ee_functions
+from GEE_ISMN import earthengine as ee
 
 
 # Initiate GEE & ask for user input
-user_input = ee_functions.setup_ee()
+user_input = ee.setup_ee()
 
 
 # Import and process location data
 geo = gpd.read_file('./data/coordinates_2.geojson')
-ee_geo = ee_functions.process_geojson(geo, user_input)
+ee_geo = ee.process_geojson(geo, user_input)
 
 
 # Import land cover data and filter the locations
-ee_geo_filt = ee_functions.lc_filter(ee_geo)
+ee_geo_filt = ee.lc_filter(ee_geo)
 
 
 # Load and filter Sentinel-1 image collection
-img_coll = ee_functions.get_image_collection(ee_geo_filt)
+img_coll = ee.get_image_collection(ee_geo_filt)
 
 
 # Extract backscatter data for each location
@@ -33,19 +33,19 @@ def ts_to_dict(img_collection, geo_list_filt):
 
 # Point
 for key in img_coll.keys():
-    s1_data = ee_functions.time_series_of_a_point(img_coll[key][1], img_coll[key][0])
+    s1_data = ee.time_series_of_a_point(img_coll[key][1], img_coll[key][0])
     s1_data.reset_index().plot(x='Dates', y='VH')
     #plt.savefig("./figs/" + str(key) + ".png")
     #plt.close()
 
 # Region
 for key in img_coll.keys():
-    s1_data = ee_functions.time_series_of_a_region(img_coll[key][1], img_coll[key][0])
+    s1_data = ee.time_series_of_a_region(img_coll[key][1], img_coll[key][0])
     s1_data.reset_index().plot(x='Dates', y='VV')
     plt.savefig("./figs/" + str(key) + "_sq50.png")
     plt.close()
 
-s1_data = ee_functions.time_series_of_a_point(img_coll["geo_1"][1], img_coll["geo_1"][0])
+s1_data = ee.time_series_of_a_point(img_coll["geo_1"][1], img_coll["geo_1"][0])
 s1_data
 
 #s1_data.reset_index().plot(x='Dates', y='VH')
