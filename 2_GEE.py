b0f211664c540d42d976eb0b3a041ed1f051a55f
maawoo <marco.wolsza@gmail.com>
2020-02-21 16:54:53 +0100

    Minor changes

--- 2_GEE.py
+++ 2_GEE.py
@@ -25,34 +25,10 @@ test_dict[data2.network + "-" + data2.sensor + "-" + data2.station] = [
     data2.latitude, data2.longitude, data2.data]
 
 test_dict_2 = earth.lc_filter(test_dict, user_input)
-test_dict_3 = earth.get_image_collection(test_dict_2)
-key = 'RSMN-5TM-Dumbraveni'
 
-s1_data = earth.time_series_of_a_point(test_dict_3[key][4], test_dict_3[
-    key][3])
-
-test_dict_3[key].append(s1_data)
-
-test_dict_3[key][6]
+test_dict_3 = earth.get_s1_backscatter(test_dict_2)
 
 #############
-
-# Point
-for key in img_coll.keys():
-    s1_data = ee.time_series_of_a_point(img_coll[key][1], img_coll[key][0])
-    s1_data.reset_index().plot(x='Dates', y='VH')
-    # plt.savefig("./figs/" + str(key) + ".png")
-    # plt.close()
-
-# Region
-for key in img_coll.keys():
-    s1_data = ee.time_series_of_a_region(img_coll[key][1], img_coll[key][0])
-    s1_data.reset_index().plot(x='Dates', y='VV')
-    plt.savefig("./figs/" + str(key) + "_sq50.png")
-    plt.close()
-
-s1_data = ee.time_series_of_a_point(img_coll["geo_1"][1], img_coll["geo_1"][0])
-s1_data
 
 # s1_data.reset_index().plot(x='Dates', y='VH')
 # plt.show()
