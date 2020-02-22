2665a88c324e395c86187df82d9cbc863a146759
maawoo <marco.wolsza@gmail.com>
2020-02-22 15:20:20 +0100

    Deleted file

--- 4_Plotting.py
+++
@@ -1,8 +0,0 @@
-
-# Allgemeine Vergleichsplots zwischen den S-1 Rückstreuwerten und den ISMN
-# Bodenfeuchtemessungen
-
-# Funktion mit der man einzelne Stationen in einer Karte anschauen kann (
-# z.B. in einem bestimmten Zeitraum merkwürdige Rückstreuwerte, die von GEE
-# kommen -> Wie sieht die entsprechende Sentinel-Szene eigentlich aus und
-# kann man dadurch die Werte erklären?)
