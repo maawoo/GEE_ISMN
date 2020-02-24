5569376942d118c92289cfd8d5cdad2372e36fd4
be76voj <patrick.fischer@uni-jena.de>
2020-02-24 23:08:23 +0100

    fixed double values in dataframes

--- GEE_ISMN/postprocessing.py
+++ GEE_ISMN/postprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # data_filter(data_dict, filter=None)
 #   - filter:
@@ -11,7 +12,12 @@ import pandas as pd
 
 
 def filter_s1_desc(data_dict):
+    """ lazy evaluation: if the entry in VH is an array set it as the first value of that array, set the normal value
+        :param
+        :type
 
+        :return:
+        """
     copy_sm = []
     plotdata = []
     for key in data_dict.keys():
@@ -20,7 +26,11 @@ def filter_s1_desc(data_dict):
         for timestamp in timeseries_sm.index:
             copy_sm.append(timestamp)
 
+        last_timestamp = None
         for timestamp in timeseries_s1.index:
+            if (last_timestamp == timestamp):
+                continue
+            last_timestamp = timestamp
             sm_series = None
             result = None
             for sm_timestamp_index in range(len(copy_sm)):
@@ -33,15 +43,19 @@ def filter_s1_desc(data_dict):
             if result is not None:
                 record = {
                     "t_s1_desc": timestamp,
-                    "VH_desc": timeseries_s1.at[timestamp, "VH"],
-                    "VV_desc": timeseries_s1.at[timestamp, "VV"],
+                    "VH_desc": timeseries_s1.at[timestamp, "VH"][0]\
+                        if isinstance(timeseries_s1.at[timestamp, "VH"], np.ndarray)\
+                        else timeseries_s1.at[timestamp, "VH"],
+                    "VV_desc": timeseries_s1.at[timestamp, "VV"][0]\
+                        if isinstance(timeseries_s1.at[timestamp, "VV"], np.ndarray)\
+                        else timeseries_s1.at[timestamp, "VV"],
                     "t_sm": copy_sm[result].tz_localize(None),
                     "sm": timeseries_sm.at[copy_sm[result], "soil moisture"]
                 }
+                print(type(record["VH_desc"]))
                 plotdata.append(record)
                 copy_sm = copy_sm[result:]
-
-        data_dict[key].append(pd.DataFrame(plotdata))
+        data_dict[key].append(pd.DataFrame(data=plotdata, dtype=np.float))
 
 
 def filter_s1_asc(data_dict):
@@ -66,13 +80,17 @@ def filter_s1_asc(data_dict):
             if result is not None:
                 record = {
                     "t_s1_asc": timestamp,
-                    "VH_asc": timeseries_s1.at[timestamp, "VH"],
-                    "VV_asc": timeseries_s1.at[timestamp, "VV"],
+                    "VH_asc": timeseries_s1.at[timestamp, "VH"][0]\
+                        if isinstance(timeseries_s1.at[timestamp, "VH"], np.ndarray)\
+                        else timeseries_s1.at[timestamp, "VH"],
+                    "VV_asc": timeseries_s1.at[timestamp, "VV"][0]\
+                        if isinstance(timeseries_s1.at[timestamp, "VV"], np.ndarray)\
+                        else timeseries_s1.at[timestamp, "VV"],
                     "t_sm": copy_sm[result].tz_localize(None),
                     "sm": timeseries_sm.at[copy_sm[result], "soil moisture"]
                 }
                 plotdata.append(record)
                 copy_sm = copy_sm[result:]
 
-        data_dict[key].append(pd.DataFrame(plotdata))
+        data_dict[key].append(pd.DataFrame(data=plotdata, dtype=np.float))
 
