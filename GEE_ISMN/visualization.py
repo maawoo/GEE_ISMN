def3101158586b4c8253bbfa20255f4ced5ab5dd
be76voj <patrick.fischer@uni-jena.de>
2020-03-02 13:27:36 +0100

    Added function for plotting the data

--- GEE_ISMN/visualization.py
+++ GEE_ISMN/visualization.py
@@ -1,6 +1,8 @@
 import ee
-import folium
+#import folium
 import webbrowser
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
 
 # vis_comparison(data_dict, station=None, timeframe=None)
 #   - station:
@@ -154,3 +156,82 @@ def _date_dist(img_coll, date):
     coll = img_coll.map(_img_iter)
 
     return coll
+
+
+def plot_data(data_dict, station_name, orbit=None, pol=None):
+
+    global plot_label, plot_pol, plot_title
+    station = station_name
+
+    if orbit is None:
+        orbit = "desc"
+    if pol is None:
+        pol = "VV"
+
+    if type(station) != str:
+        raise TypeError("The input for the parameter \"station\" should be "
+                        "of type \"str\".")
+    try:
+        data_dict[station]
+    except KeyError:
+        print("The station " + str(station) + " was not found in the input "
+                                              "dictionary.")
+
+    if pol not in ["VV", "VH"]:
+        raise ValueError("Only \"VV\" or \"VH\" are valid options for the "
+                         "input parameter \"pol\"!")
+    elif type(pol) != str:
+        raise TypeError("The input for the parameter \"pol\" should be of "
+                        "type \"str\". Valid options are: \"VV\" or \"VH\".")
+    if orbit == "desc":
+        plot_date = data_dict[station][8].t_s1_desc
+        plot_soil = data_dict[station][8].sm
+        if pol == "VV":
+            plot_pol = data_dict[station][8].VV_desc
+            plot_label = "VV - Descending"
+            plot_title = "ISMN Soil Moisture against Sentinel-1 VV (Descending Orbit)"
+        elif pol == "VH":
+            plot_pol = data_dict[station][8].VH_desc
+            plot_label = "VH - Descending"
+            plot_title = "ISMN Soil Moisture against Sentinel-1 VH (Descending Orbit)"
+
+    elif orbit == "asc":
+        plot_date = data_dict[station][9].t_s1_asc
+        plot_soil = data_dict[station][9].sm
+        if pol == "VV":
+            plot_pol = data_dict[station][9].VV_asc
+            plot_label = "VV - Ascending"
+            plot_title = "ISMN Soil Moisture against Sentinel-1 VV (Ascending Orbit)"
+        elif pol == "VH":
+            plot_pol = data_dict[station][9].VH_asc
+            plot_label = "VH - Ascending"
+            plot_title = "ISMN Soil Moisture against Sentinel-1 VH (Ascending Orbit)"
+    else:
+        raise ValueError("Only \"desc\" (= descending) or \"asc\" (= "
+                         "ascending) are valid options for the input "
+                         "parameter \"orbit\"!")
+    if type(orbit) != str:
+        raise TypeError("The input for the parameter \"orbit\" should be of "
+                        "type \"str\". Valid options are: \"desc\" or "
+                        "\"asc\".")
+
+    register_matplotlib_converters()
+    fig, plot1 = plt.subplots(figsize=(14, 8))
+    plot_s1 = plot1.plot(plot_date, plot_pol, color='red', label=plot_label)
+
+    plot1.set_xlabel("Year", fontsize=14)
+    plot1.set_ylabel("dB", fontsize=14)
+
+    plot2 = plot1.twinx()
+    plot_sm = plot2.plot(plot_date, plot_soil, color='blue', dashes=[6, 3], label="Soil Moisture")
+    plot2.set_ylabel("Soil Moisture", fontsize=14)
+
+    plot1.xaxis.set_major_locator(plt.MaxNLocator(10))
+    plot1.tick_params(axis='x', labelrotation=-45)
+    plots = plot_s1 + plot_sm
+    labels = [l.get_label() for l in plots]
+    plot1.legend(plots, labels, bbox_to_anchor=(1.15, 1), loc='upper left', borderaxespad=0.)
+    plt.title(plot_title, fontsize=20)
+    fig.tight_layout()
+    plt.show()
+
