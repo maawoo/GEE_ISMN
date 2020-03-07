1265bf965ac8b29a8daf9b316cfc5107594fe883
maawoo <marco.wolsza@gmail.com>
2020-03-07 21:58:03 +0100

    Code structure & Grammar

--- GEE_ISMN/postprocess.py
+++ GEE_ISMN/postprocess.py
@@ -2,29 +2,26 @@ import pandas as pd
 import numpy as np
 
 
-def filter_s1(data_dict):
+def ts_filter(data_dict):
+    """Filters the ISMN data by comparing the timestamp of each soil
+    moisture measurement with the recording time of each Sentinel-1
+    backscatter value from ascending & descending orbits.
+    Only soil moisture values that were measured right before the
+    S-1 datapoint are copied into a new dataframe. This function also checks
+    for duplicate Sentinel-1 timestamps. In case of duplicates, only the
+    first entry will be used.
 
-    def filter_s1_desc(data_dict_desc):
-        """ This function filters the ISMN data by comparing
-        time of recording of ISMN and Sentinel-1 data from the
-        Descending Orbit. Only ISMN data which were collected
-        right before the Sentinel-1 data should be used.
-        Also the function checks for double Sentinel-1 timestamps.
-        In case of doubled Sentinel-1 dcenes for one timestamp,
-        the first one will be choosen.
-            :param data_dict_desc: dictionary which contains the following values:
-                - Sentinel-1 timestamps of descending orbit
-                - Sentinel-1 backscatter values (VH/VV) of descending orbit
-                - ISMN timestamps
-                - ISMN soil moisture values
+    :param data_dict: Dictionary that was created/modified by using the
+    function get_s1_backscatter(). Must contain a dataframe for the ISMN soil
+    moisture data and dataframes for the Sentinel-1 backscatter data
+    (descending & ascending orbits) for each dictionary key.
+        :type: Dictionary
 
-            :type data_dict_desc: dictionary
+    :return: Dictionary with added dataframe that contains the filtered soil
+    moisture and backscatter data.
+    """
 
-            :return: a dataframe containing the Sentinel-1 Timestamps
-            (t_s1_desc) and backscatter values (VH_desc & VV_desc) for the
-            descending Orbit and the assigned ISMN soil moisture values (sm)
-            and timestamps (t_sm)
-            """
+    def _filter_s1_desc(data_dict_desc):
 
         for key in data_dict_desc.keys():
             if data_dict_desc[key][6] is None:
@@ -40,7 +37,7 @@ def filter_s1(data_dict):
 
                 last_timestamp = None
                 for timestamp in timeseries_s1.index:
-                    if (last_timestamp == timestamp):
+                    if last_timestamp == timestamp:
                         continue
                     last_timestamp = timestamp
                     sm_series = None
@@ -56,40 +53,25 @@ def filter_s1(data_dict):
                         record = {
                             "t_s1_desc": timestamp,
                             "VH_desc": timeseries_s1.at[timestamp, "VH"][0] \
-                                if isinstance(timeseries_s1.at[timestamp, "VH"], np.ndarray) \
+                                if isinstance(timeseries_s1.at[timestamp, "VH"],
+                                              np.ndarray) \
                                 else timeseries_s1.at[timestamp, "VH"],
                             "VV_desc": timeseries_s1.at[timestamp, "VV"][0] \
-                                if isinstance(timeseries_s1.at[timestamp, "VV"], np.ndarray) \
+                                if isinstance(timeseries_s1.at[timestamp, "VV"],
+                                              np.ndarray) \
                                 else timeseries_s1.at[timestamp, "VV"],
                             "t_sm": copy_sm[result].tz_localize(None),
                             "sm": timeseries_sm.at[copy_sm[result], "soil moisture"]
                         }
+
                         plotdata.append(record)
                         copy_sm = copy_sm[result:]
+
                 data_dict_desc[key].append(pd.DataFrame(data=plotdata, dtype=np.float))
+
         return data_dict
 
-    def filter_s1_asc(data_dict_asc):
-        """ This function filters the ISMN Data by comparing
-            time of recording of ISMN and Sentinel-1 data from the
-            Ascending Orbit. Only ISMN Data which were collected
-            right before the Sentinel-1 Data should be used.
-            Also the function checks for double Sentinel-1 timestamps.
-            In case of doubled Sentinel-1 Scenes for one timestamp,
-            the first one will be choosen.
-                :param data_dict_asc: dictionary which contains the following values:
-                    - Sentinel-1 Timestamps of Ascending Orbit
-                    - Sentinel-1 Backscatter Values (VH/VV) of Ascending Orbit
-                    - ISMN Timestamps
-                    - ISMN Soil moisture Values
-
-                :type data_dict_asc: dictionary
-
-                :return: a Dataframe containing the Sentinel-1 Timestamps
-                (t_s1_asc) and Backscatter Values (VH_asc & VV_asc) for the
-                descending Orbit and the assigned ISMN Soil moisture Values (sm)
-                and Timestamps (t_sm)
-                """
+    def _filter_s1_asc(data_dict_asc):
 
         for key in data_dict_asc.keys():
             if data_dict_asc[key][7] is None:
@@ -105,7 +87,7 @@ def filter_s1(data_dict):
 
                 last_timestamp_asc = None
                 for timestamp in timeseries_s1.index:
-                    if (last_timestamp_asc == timestamp):
+                    if last_timestamp_asc == timestamp:
                         continue
                     last_timestamp_asc = timestamp
                     sm_series = None
@@ -129,14 +111,16 @@ def filter_s1(data_dict):
                             "t_sm": copy_sm[result].tz_localize(None),
                             "sm": timeseries_sm.at[copy_sm[result], "soil moisture"]
                         }
+
                         plotdata.append(record)
                         copy_sm = copy_sm[result:]
 
                 data_dict_asc[key].append(pd.DataFrame(data=plotdata, dtype=np.float))
+
         return data_dict
 
-    filter_s1_desc(data_dict)
-    filter_s1_asc(data_dict)
+    _filter_s1_desc(data_dict)
+    _filter_s1_asc(data_dict)
 
     return data_dict
 
